aa8f1b1825a7c3c82290f40ca4c33f23e0a60d1f
Marco Schneider <marco.schneider@active-group.de>
2022-04-06 17:52:48 +0200

    Add all platforms explicitly and hope for the best I guess.

--- make-gemlock.sh
+++ make-gemlock.sh
@@ -3,6 +3,6 @@
 nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/refs/tags/21.11.tar.gz \
 	  -p bundler \
 	  -p bundix \
-	  --run 'bundler update; bundler lock; bundler package --no-install --path vendor; bundix; rm -rf vendor'
+	  --run 'bundler update; bundler lock --add-platform x86_64-linux --add-platform x86_64-darwin; bundler package --no-install --path vendor; bundix; rm -rf vendor'
 
 echo "You can now run nix-shell"
