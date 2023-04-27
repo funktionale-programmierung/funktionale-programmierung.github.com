6c4ac1a577d4acfd0ea61421adc5047c32314de3
Johannes Maier <johannes.maier@active-group.de>
2023-04-27 17:58:21 +0200

    Zap unused files

--- make-gemlock.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/refs/tags/21.11.tar.gz \
-	  -p bundler \
-	  -p bundix \
-	  --run 'bundler update; bundler lock --add-platform x86_64-linux --add-platform x86_64-darwin; bundler package --no-install --path vendor; bundix; rm -rf vendor'
-
-echo "You can now run nix-shell"
