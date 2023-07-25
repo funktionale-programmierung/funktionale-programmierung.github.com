92b0151bf51a8982a4c8cb1f5a4e68c8ef449c2e
Johannes Maier <johannes.maier@active-group.de>
2023-07-25 14:34:43 +0200

    flake: update description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "Commands for developing the funktionale-programmierung.de website";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
